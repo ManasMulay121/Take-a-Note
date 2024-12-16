3915576d0a6ebea27e7d8751f1d8713fc34d6c8b
Manas Mulay <mulaymp23.comp@coeptech.ac.in>
2024-12-16 17:36:29 +0530

    added files for blog/notes app

---
+++ app.js
@@ -0,0 +1,44 @@
+let posts = [];
+
+var bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    // res.status(200);
+    res.render(path.join(__dirname + '/views/home.ejs'));
+})
+
+app.get('/createBlog', (req, res)=> {
+    res.render(path.join(__dirname + '/views/create_page.ejs'))
+})
+
+app.get('/viewPost/:id', (req, res) => {
+    let postNo = req.params.id - 1;
+    const title = posts[postNo][1].title;
+    const blog = posts[postNo][1].blog;
+    let post = {title, blog};
+    res.render(path.join(__dirname + '/views/view_post.ejs'), {post});
+})
+
+app.post("/postBlog", (req, res) => {
+    const len = posts.length;
+    const title = req.body["Title"];
+    const blog = req.body["Blog"];
+    posts.push([len + 1, {title, blog}]);
+    console.log(posts[posts.length - 1][1].title);
+    res.render(path.join(__dirname + '/views/home.ejs'), {posts});
+})
+
+app.post("/deletePost", (req, res) => {
+    
+})
+
+app.listen(port, () => {
+    console.log("i'm all ears daddy, uhmmmmm :>");
+})
